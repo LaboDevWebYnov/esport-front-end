d3a8516644d51147ec4275fd1c3a589ad54fdbac
amaitre <antoine.maitre@ynov.com>
2017-01-11 11:11:06 +0100

    [app.module] refactored /profil -> /profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,22 @@ import {TestUserServiceComponent} from './component/test_service_component/test-
 import {TestTeamServiceComponent} from './component/test_service_component/test-team-service/test-team-service.component';
 import {TestSecurityServiceComponent} from './component/test_service_component/test-security-service/test-security-service.component';
 import {TestPlayerAccountServiceComponent} from './component/test_service_component/test-player-account-service/test-player-account-service.component';
-import { TestGameServiceComponent } from './component/test_service_component/test-game-service/test-game-service.component';
-import { TestAddressServiceComponent } from './component/test_service_component/test-address-service/test-address-service.component';
-import { MenuComponent } from './component/shared/menu/menu.component';
-import { LogoComponent } from './component/shared/logo/logo.component';
-import { NewsComponent } from './component/news/news/news.component';
-import { HomeComponent } from './component/home/home/home.component';
-import { EventsComponent } from './component/events/events/events.component';
-import { StreamingsComponent } from './component/streamings/streamings/streamings.component';
-import { ProfileUserComponent } from './component/profile-user/profile-user.component';
-import { UserBannerComponent } from './component/profile-user/user-banner/user-banner.component';
-import { UserSocialComponent } from './component/profile-user/user-social/user-social.component';
-import { UserGamesComponent } from './component/profile-user/user-games/user-games.component';
-import { UserResultsComponent } from './component/profile-user/user-results/user-results.component';
-import { UserHistoricComponent } from './component/profile-user/user-historic/user-historic.component';
-import { UserNewsComponent } from './component/profile-user/user-news/user-news.component';
-import { ModalProfilModificationComponent } from './component/profile-user/modal-profil-modification/modal-profil-modification.component';
+import {TestGameServiceComponent} from './component/test_service_component/test-game-service/test-game-service.component';
+import {TestAddressServiceComponent} from './component/test_service_component/test-address-service/test-address-service.component';
+import {MenuComponent} from './component/shared/menu/menu.component';
+import {LogoComponent} from './component/shared/logo/logo.component';
+import {NewsComponent} from './component/news/news/news.component';
+import {HomeComponent} from './component/home/home/home.component';
+import {EventsComponent} from './component/events/events/events.component';
+import {StreamingsComponent} from './component/streamings/streamings/streamings.component';
+import {ProfileUserComponent} from './component/profile-user/profile-user.component';
+import {UserBannerComponent} from './component/profile-user/user-banner/user-banner.component';
+import {UserSocialComponent} from './component/profile-user/user-social/user-social.component';
+import {UserGamesComponent} from './component/profile-user/user-games/user-games.component';
+import {UserResultsComponent} from './component/profile-user/user-results/user-results.component';
+import {UserHistoricComponent} from './component/profile-user/user-historic/user-historic.component';
+import {UserNewsComponent} from './component/profile-user/user-news/user-news.component';
+import {ModalProfilModificationComponent} from './component/profile-user/modal-profil-modification/modal-profil-modification.component';
 
 
 @NgModule({
@@ -84,7 +84,7 @@ import { ModalProfilModificationComponent } from './component/profile-user/modal
         component: TestUserServiceComponent
       },
       {
-        path: 'profil',
+        path: 'profile',
         component: ProfileUserComponent
       },
       {
