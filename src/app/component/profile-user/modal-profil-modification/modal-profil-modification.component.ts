b392e0b3306a5124b4b74995462894b3dba2cac5
Florian Billion <florian.billion@ynov.com>
2017-01-11 10:11:14 +0100

    [profil-user]Modification page, début de séance 11/01/17

--- src/app/component/profile-user/modal-profil-modification/modal-profil-modification.component.ts
+++ src/app/component/profile-user/modal-profil-modification/modal-profil-modification.component.ts
@@ -11,5 +11,4 @@ export class ModalProfilModificationComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
--- src/app/component/profile-user/user-banner/user-banner.component.ts
+++ src/app/component/profile-user/user-banner/user-banner.component.ts
@@ -3,11 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-banner',
   templateUrl: './user-banner.component.html',
-  styleUrls: ['./user-banner.component.css']
+  styleUrls: ['./user-banner.component.css'],
+
 })
 export class UserBannerComponent implements OnInit {
 
   constructor() { }
+
+  modifProfil(){
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+  }
+  closeModal(){
+    var modal = document.getElementById('myModal');
+    modal.style.display = "none";
+  }
 
   ngOnInit() {
   }
