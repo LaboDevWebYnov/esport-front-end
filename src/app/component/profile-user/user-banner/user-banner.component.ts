200b68d7a6be4d37b754995bcfec10d6f5c9fc43
BouleWayne <benjamin.lhonnen@ynov.com>
2017-02-08 09:00:00 +0100

    [profile]  add supp address + modif address

--- src/app/component/profile-user/user-banner/user-banner.component.ts
+++ src/app/component/profile-user/user-banner/user-banner.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { Configuration } from '../../../../shared/app.constants';
 import { AddressService } from "../../../../shared/services/address.service";
+import {Address, DisableAddress} from '../../../../shared/models/address';
+import {User} from '../../../../shared/models/user';
 import { UserService } from '../../../../shared/services/user.service';
+
+import { CoolLocalStorage } from 'angular2-cool-storage';
 
 @Component({
   selector: 'app-user-banner',
@@ -12,20 +16,26 @@ import { UserService } from '../../../../shared/services/user.service';
 
 })
 export class UserBannerComponent implements OnInit {
-
+  localStorage: CoolLocalStorage;
   private addressApiJson: Object;
   private userGetById: Object;
+  private addressUpdate:Address;
+  private reponse:Object;
+  private disableAddress:DisableAddress;
+  private user:User;
 
   constructor(private addressServiceInstance: AddressService,
-              private userServiceInstance: UserService) { }
+              private userServiceInstance: UserService,
+              localStorage: CoolLocalStorage
+              ){
+              this.localStorage = localStorage;
+              }
 
-  modifProfil(){
-    var modal = document.getElementById("myModal");
-    modal.style.display = "block";
+  public modifProfil() :void{
+    (<HTMLElement>document.getElementById("myModal")).style.display = "block";
   }
-  closeModal(){
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none";
+  public closeModal() :void{
+    (<HTMLElement>document.getElementById("myModal")).style.display = "none";
   }
 
   private getItemAddressById(idUser: string): void{
@@ -48,10 +58,94 @@ export class UserBannerComponent implements OnInit {
       );
   }
 
-  private deleteAddress(){}
+  private deleteAddress(addressid:string,liId:number): void {
+  let liDelete = (<HTMLElement>document.getElementById("address"+liId));
+  liDelete.style.display = "none";
+  this.disableAddress={active:false};
+  this.addressServiceInstance
+    .DisableAddressByAddressId(addressid,this.disableAddress)
+    .subscribe(
+      data => this.reponse = data,
+      error => console.log(error),
+      () => console.log('get One Address by ID complete', this.reponse)
+    );
+}
+
+
+  private unlockAddress(inputsiI:number): void {
+    let inputLine = (<HTMLInputElement>document.getElementById("inputLine"+inputsiI));
+    let inputCity = (<HTMLInputElement>document.getElementById("inputCity"+inputsiI));
+    let inputCountry = (<HTMLInputElement>document.getElementById("inputCountry"+inputsiI));
+    let inputPostCode = (<HTMLInputElement>document.getElementById("inputPostCode"+inputsiI));
+    let pencil = (<HTMLInputElement>document.getElementById("pencil"+inputsiI));
+    let check = (<HTMLInputElement>document.getElementById("check"+inputsiI));
+
+
+    inputLine.removeAttribute("disabled");
+    inputCity.removeAttribute("disabled");
+    inputCountry.removeAttribute("disabled");
+    inputPostCode.removeAttribute("disabled");
+
+    pencil.style.display = "none";
+    check.style.display = "inline";
+  }
+
+
+  private updateAddress(inputsID:number,addressId:string): void {
+    let inputLine = (<HTMLInputElement>document.getElementById("inputLine"+inputsID));
+    let inputCity = (<HTMLInputElement>document.getElementById("inputCity"+inputsID));
+    let inputCountry = (<HTMLInputElement>document.getElementById("inputCountry"+inputsID));
+    let inputPostCode = (<HTMLInputElement>document.getElementById("inputPostCode"+inputsID));
+    let pencil = (<HTMLInputElement>document.getElementById("pencil"+inputsID));
+    let check = (<HTMLInputElement>document.getElementById("check"+inputsID));
+
+
+    inputLine.setAttribute("disabled","true");
+    inputCity.setAttribute("disabled","true");
+    inputCountry.setAttribute("disabled","true");
+    inputPostCode.setAttribute("disabled","true");
+
+    pencil.style.display = "inline";
+    check.style.display = "none";
+
+    console.log(inputPostCode.value);
+    console.log(Number(inputPostCode.value));
+    console.log(parseInt(inputPostCode.value));
+
+    this.addressUpdate = {
+      postCode: Number(inputPostCode.value),
+      city: inputCity.value,
+      country: inputCountry.value,
+      line: inputLine.value,
+    };
+
+    this.addressServiceInstance
+      .UpdateAddressById(this.localStorage.getItem('userId'),addressId,this.addressUpdate)
+      .subscribe(
+        data => this.reponse = data,
+        error => console.log(error),
+        () => console.log('get One Address by ID complete', this.reponse)
+      );
+
+
+  }
+
+  private updateProfile(id:string):void{
+    // this.user = {
+    //
+    // };
+    this.userServiceInstance
+      .ChangeUserInformation(id,this.user)
+      .subscribe(
+        data => this.userGetById = data,
+        error => console.log(error),
+        () => console.log('get One Item complete', this.userGetById)//console.log('get All Items complete')
+      );
+  }
+
 
   ngOnInit() {
-    var id = "569000574367285c00961282";/*583a9d3b95ecb33490f49896*//*569000574367285c00961282*/
+    const id = this.localStorage.getItem('userId');
     this.getItemAddressById(id);
     this.getItemUserById(id);
   }
