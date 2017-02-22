afa1a26d3d6a634af2764d34bab05dfab9ef98c2
Florian Billion <florian.billion@ynov.com>
2017-02-22 13:35:06 +0100

    [TEAM] Modif page Team

--- src/app/component/team/team/team.component.ts
+++ src/app/component/team/team/team.component.ts
@@ -1,26 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from '../../../../shared/services/team.service';
+import { Configuration } from '../../../../shared/app.constants';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
+  providers: [TeamService, Configuration]
 })
 export class TeamComponent implements OnInit {
+  private teams: Object;
 
-  constructor() { }
+  constructor(private teamServiceInstance: TeamService,) { }
 
   ngOnInit() {
   }
 
   display(id:string):void {
-    if (document.getElementById(id).style.maxHeight=="0px")
-    {
-      document.getElementById(id).style.maxHeight="5000px";
-    }
-    else
-    {
+    if (document.getElementById(id).style.maxHeight=="0px") {
+      document.getElementById(id).style.maxHeight="";
+      id += "Arrow";
+      var elmt = document.getElementById(id);
+      elmt.style.webkitTransform="rotate(0deg)";
+      elmt.style.transform="rotate(0deg)";
+    } else {
       document.getElementById(id).style.maxHeight="0px";
+      id += "Arrow";
+      var elmt = document.getElementById(id);
+      elmt.style.webkitTransform="rotate(180deg)";
+      elmt.style.transform="rotate(180deg)";
     }
+    // elmt.="rotate(0deg)";
+    // elmt.style.webkitTransform="rotate(0deg)";
+    // elmt.style.webkitTransform="rotate(0deg)";
+
+    // -moz-transform: rotate(180deg);
+    // -o-transform: rotate(180deg);
+    // -ms-transform: rotate(180deg);
   }
 
+  onSubmit(event) {
+    console.log("Recherche : "+ event.target[0].value);
+    this.searchTeams(event.target[0].value);
+  }
+
+  private searchTeams(team:string){
+    this.teamServiceInstance
+      .GetSingleTeamByName(team)
+      .subscribe(
+        data => this.teams = data,
+        error => console.log(error),
+        () => {/*console.log('getSingleTeamByName', this.teams)*/}
+      );
+
+  }
 }
