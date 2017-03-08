d4328cc1f1f1f9b736349182407663aece805e3c
Thomas Doret <thomas.doret33@gmail.com>
2017-03-08 15:13:43 +0100

    [STREAM/NEWS] add template for the news & streaming display

--- src/app/component/news/news/news.component.ts
+++ src/app/component/news/news/news.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {GameService} from "../../../../shared/services/game.service";
+import {Configuration} from "../../../../shared/app.constants";
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+  providers: [Configuration,GameService]
+
 })
 export class NewsComponent implements OnInit {
+  private games: Object;
+  public isFilteredGameId: any;
 
-  constructor() { }
+  constructor(private gameServiceInstance: GameService
+  ) { }
 
   ngOnInit() {
+    this.getGames();
+  }
+
+  public checkFilter(gameIdFiltered: string)
+  {
+    this.isFilteredGameId = gameIdFiltered;
+  }
+
+  private getGames(): void {
+    this.gameServiceInstance
+      .GetAllGames()
+      .subscribe(
+        data => this.games = data,
+        error => console.log(error),
+        () => {/*console.log('get all games complete', this.games)*/}
+      );
   }
 
 }
--- src/app/component/streamings/streamings/streamings.component.ts
+++ src/app/component/streamings/streamings/streamings.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {GameService} from "../../../../shared/services/game.service";
+import {Configuration} from "../../../../shared/app.constants";
 
 @Component({
   selector: 'app-streamings',
   templateUrl: './streamings.component.html',
-  styleUrls: ['./streamings.component.css']
+  styleUrls: ['./streamings.component.css'],
+  providers: [Configuration,GameService]
+
 })
 export class StreamingsComponent implements OnInit {
+  private games: Object;
+  public isFilteredGameId: any;
 
-  constructor() { }
+  constructor(private gameServiceInstance: GameService
+  ) { }
 
   ngOnInit() {
+    this.getGames();
   }
 
+  public checkFilter(gameIdFiltered: string)
+  {
+    this.isFilteredGameId = gameIdFiltered;
+  }
+
+  private getGames(): void {
+    this.gameServiceInstance
+      .GetAllGames()
+      .subscribe(
+        data => this.games = data,
+        error => console.log(error),
+        () => {/*console.log('get all games complete', this.games)*/}
+      );
+  }
 }
