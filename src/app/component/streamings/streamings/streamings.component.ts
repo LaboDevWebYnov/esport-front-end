884eb8302f5218056a69b69951e865c3a397260a
Thomas Doret <thomas.doret33@gmail.com>
2017-04-05 14:50:12 +0200

    [streaming] liveStreamDisplay and searchStreamDisplay

--- src/app/component/streamings/streamings/streamings.component.ts
+++ src/app/component/streamings/streamings/streamings.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {GameService} from "../../../../shared/services/game.service";
+import {StreamingService} from '../../../../shared/services/streaming.service';
 import {Configuration} from "../../../../shared/app.constants";
 
 @Component({
   selector: 'app-streamings',
   templateUrl: './streamings.component.html',
   styleUrls: ['./streamings.component.css'],
-  providers: [Configuration,GameService]
+  providers: [Configuration,GameService, StreamingService]
 
 })
 export class StreamingsComponent implements OnInit {
   private games: Object;
+  private liveStream: Object;
+  private searchStream: Object;
   public isFilteredGameId: any;
 
-  constructor(private gameServiceInstance: GameService
-  ) { }
+  constructor(private gameServiceInstance: GameService,
+              private streamingServiceInstance: StreamingService
+  ) {
+    this.getLiveStream();
+  }
 
   ngOnInit() {
     this.getGames();
@@ -23,6 +29,34 @@ export class StreamingsComponent implements OnInit {
   public checkFilter(gameIdFiltered: string)
   {
     this.isFilteredGameId = gameIdFiltered;
+  }
+
+  public onChangeSearchStream(event) {
+    console.log(event.target.value);
+    if(event.target.value.length > 0)
+      this.getStreamByLikeName(event.target.value);
+    else
+      this.searchStream = null;
+  }
+
+  private getLiveStream(): void {
+    this.streamingServiceInstance
+      .getLiveStreaming()
+      .subscribe(
+        data => this.liveStream = data,
+        error => console.log(error),
+        () => {console.log('get live stream complete', this.liveStream)}
+      )
+  }
+
+  private getStreamByLikeName(twitchUser: string): any {
+    this.streamingServiceInstance
+      .getStreamingByLikeName(twitchUser)
+      .subscribe(
+        data => this.searchStream = data,
+        error => console.log(error),
+        () => {console.log('get stream complete', this.searchStream)}
+      )
   }
 
   private getGames(): void {
---
+++ src/shared/services/streaming.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable';
+import { Configuration } from '../app.constants';
+
+@Injectable()
+export class StreamingService {
+
+  private actionUrl: string;
+  private headers: Headers;
+
+  constructor(private _http: Http, private _configuration: Configuration) {
+    this.actionUrl = _configuration.ServerWithApiUrl;
+
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Accept', 'application/json');
+
+  }
+
+  public getLiveStreaming = (): Observable<String> => {
+    console.log(this.actionUrl);
+    return this._http.get(this.actionUrl + 'streaming/getLiveStreaming')
+      .map(response => response.json());
+  };
+
+  public getStreamingByName = (twitchUser: string): Observable<String> => {
+    console.log(this.actionUrl);
+    return this._http.get(this.actionUrl + 'streaming/'+twitchUser+'/getStreamingByName')
+      .map(response => response.json());
+  };
+
+  public getStreamingByLikeName = (partialTwitchUser: string): Observable<String> => {
+    console.log(this.actionUrl);
+    return this._http.get(this.actionUrl + 'streaming/'+ partialTwitchUser +'/getStreamingByLikeName')
+      .map(response => response.json());
+  };
+
+}
