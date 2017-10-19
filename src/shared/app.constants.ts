99347900281b4daf9f9a930c1f25714e4ae07c2a
sylvainBrunet <sylvain69740@gmail.com>
2017-10-19 13:27:09 +0200

    server config local

--- src/shared/app.constants.ts
+++ src/shared/app.constants.ts
@@ -5,8 +5,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-  // public Server: string = "http://localhost:3100/";
-  public Server: string = "http://integration.noname-gaming.fr:3100/";
+
+   public Server: string = "http://localhost:3100/";
+ // public Server: string = "http://integration.noname-gaming.fr:3100/";
   public ApiUrl: string = "api/";
   public ServerWithApiUrl = this.Server + this.ApiUrl;
 }
