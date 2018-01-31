47efe3366c7810cbfdfa409e893d10500705a975
Florian <florian.billion@ynov.com>
2018-01-31 17:27:03 +0100

    Modif Team Profil

--- src/app/component/profile-user/profile-user.component.ts
+++ src/app/component/profile-user/profile-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-user',
-  templateUrl: 'profile-user.component.html',
+  templateUrl: './profile-user.component.html',
   styleUrls: ['profile-user.component.css']
 })
 export class ProfileUserComponent implements OnInit {
@@ -10,6 +10,23 @@ export class ProfileUserComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public changeOnglet(event, option): void {
+    let i, tabContent, tabLinks;
+
+    tabContent = document.getElementsByClassName("content");
+    for (i = 0; i < tabContent.length; i++) {
+      (<HTMLInputElement>tabContent[i]).style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabLinks.length; i++) {
+      (<HTMLInputElement>tabLinks[i]).className = tabLinks[i].className.replace(" active", "");
+    }
+
+    (<HTMLInputElement>document.getElementById(option)).style.display = "block";
+    event.currentTarget.className += " active";
   }
 
 }
