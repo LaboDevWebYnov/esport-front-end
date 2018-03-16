e4a74152f01fcae70e62476dc80f6df0adcbc245
Alexandre <alexandre.gelin71@gmail.com>
2018-03-16 11:19:13 +0100

    Fin page match en dur

--- src/app/component/tournament/details-tournament/matches/matches.component.ts
+++ src/app/component/tournament/details-tournament/matches/matches.component.ts
@@ -25,6 +25,84 @@ export class MatchesComponent implements OnInit {
       joueur1 : "Florian",
       joueur2 : "Jean-Eude DeLarue",
     },
+    {
+      joueur1 : "Sylvain",
+      joueur2 : "Alexandre",
+    },
+    {
+      joueur1 : "Clément",
+      joueur2 : "Maxence",
+    },
+    {
+      joueur1 : "Jade",
+      joueur2 : "Gauthier",
+    },
+    {
+      joueur1 : "Florian",
+      joueur2 : "Jean-Eude DeLarue",
+    },
+    {
+      joueur1 : "Florian",
+      joueur2 : "Jean-Eude DeLarue",
+    },
+
+  ];
+
+  public matchPass = [
+    {
+      joueur1 : "Sylvain",
+      score1: "7",
+      joueur2 : "Alexandre",
+      score2 : "5"
+    },
+    {
+      joueur1 : "Clément",
+      score1 : "10",
+      joueur2 : "Maxence",
+      score2 : "0"
+    },
+    {
+      joueur1 : "Jade",
+      score1 : "2",
+      joueur2 : "Gauthier",
+      score2 : "1"
+    },
+    {
+      joueur1 : "Florian",
+      score1 : "6",
+      joueur2 : "Jean-Eude DeLarue",
+      score2 : "2"
+    },
+    {
+      joueur1 : "Sylvain",
+      score1 :"1",
+      joueur2 : "Alexandre",
+      score2 : "7",
+    },
+    {
+      joueur1 : "Clément",
+      score1 : "8",
+      joueur2 : "Maxence",
+      score2 : "3",
+    },
+    {
+      joueur1 : "Jade",
+      score1 : "3",
+      joueur2 : "Gauthier",
+      score2 : "5",
+    },
+    {
+      joueur1 : "Florian",
+      score1 : "4",
+      joueur2 : "Jean-Eude DeLarue",
+      score2 : "3",
+    },
+    {
+      joueur1 : "Florian",
+      score1 : "8",
+      joueur2 : "Jean-Eude DeLarue",
+      score2 : "2",
+    },
 
   ];
 
@@ -33,8 +111,8 @@ export class MatchesComponent implements OnInit {
   ngOnInit() {
 
   }
-  ngAfterContentInit(){
+  /*ngAfterContentInit(){
     document.querySelector('.containerMatch').removeAttribute('_ngcontent-c8')
-  }
+  }*/
 
 }
