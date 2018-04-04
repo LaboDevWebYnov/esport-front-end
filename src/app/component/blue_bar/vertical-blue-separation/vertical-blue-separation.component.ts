567f0ea28f857b2886c3d74e9025a89d0cd4f3ae
Alexandre Honoré <Alexandre@MacBook-Pro-de-Alexandre-2.local>
2018-04-04 13:43:55 +0200

    creation component

---
+++ src/app/component/blue_bar/vertical-blue-separation/vertical-blue-separation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-blue-separation',
+  templateUrl: './vertical-blue-separation.component.html',
+  styleUrls: ['./vertical-blue-separation.component.css']
+})
+export class VerticalBlueSeparationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
