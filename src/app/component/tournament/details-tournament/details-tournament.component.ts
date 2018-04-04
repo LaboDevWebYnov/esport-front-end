83679c316949376973299dfa3d6ccef92862d10a
Alexandre <alexandre.gelin71@gmail.com>
2018-04-04 12:12:09 +0200

    deplacement totale des info generales de tournois

--- src/app/component/tournament/details-tournament/details-tournament.component.ts
+++ src/app/component/tournament/details-tournament/details-tournament.component.ts
@@ -26,7 +26,10 @@ export class DetailsTournamentComponent implements OnInit, AfterViewChecked {
     PlaceLoked: "12",
   };
 
-
+  public toornamentObj: object;
+  public toornamentCountry: string;
+  public participants: object;
+  tournamentId: string;
 
   public changeOnglet(event, option): void {
 
@@ -46,14 +49,39 @@ export class DetailsTournamentComponent implements OnInit, AfterViewChecked {
   }
 
   ngOnInit() {
-
+    this.tournamentId = this.route.snapshot.params['toornamentId'];
+    this.getInformationsByToornaments(this.tournamentId);
+    this.tournamentId = this.route.snapshot.params['tournamentId'];
+    this.getParticipantsByToornaments(this.tournamentId);
   }
 
   ngAfterViewChecked(){
     // document.querySelector('.content').innerHTML = document.getElementById("informations").innerHTML;
   }
 
+  private getInformationsByToornaments(tournamentid: string)
+  {
+    this.toornamentService.getTournamentById(tournamentid)
+      .subscribe(
+        data => this.toornamentObj = data,
+        error => console.log(error),
+        () => {
+          console.log("on a get le tournois", this.toornamentObj);
+        }
+      );
+  }
 
+  private getParticipantsByToornaments(tournamentid: string)
+  {
+    this.toornamentService.getParticipantsByTournament(tournamentid, [])
+      .subscribe(
+        data => this.participants = data,
+        error => console.log(error),
+        () => {
+          console.log("on a get les participant", this.participants);
+        }
+      );
+  }
 
 }
 
