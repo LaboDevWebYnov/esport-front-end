4026b7d9422ebeebc3d37973bc8e0bf056257b95
sylvainBrunet <sylvain69740@gmail.com>
2018-04-12 14:51:29 +0200

    home_connected

---
+++ src/app/component/home_connected/tournois-connected/tournois-connected.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tournois-connected',
+  templateUrl: './tournois-connected.component.html',
+  styleUrls: ['./tournois-connected.component.css']
+})
+export class TournoisConnectedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
