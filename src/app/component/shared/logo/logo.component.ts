f5d61de23837bc32c56105b0fc1cc7d357f2542d
TakamakWasTaken <gautier.nicollet@ynov.com>
2018-04-26 11:32:09 +0200

    menu update, now working!

--- src/app/component/shared/logo/logo.component.ts
+++ src/app/component/shared/logo/logo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {_document} from "@angular/platform-browser/src/browser";
 
 @Component({
   selector: 'app-logo',
@@ -10,6 +11,14 @@ export class LogoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public logoClick(event): void{
+    var activeOnglet = document.getElementsByClassName("activePage");
+    activeOnglet[0].classList.remove("activePage");
+
+    var homeButton = document.getElementsByClassName("home");
+    homeButton[0].classList.add("activePage");
   }
 
 }
